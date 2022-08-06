8bdb789f2d7147b20a88c3bf60ef47b467f439ec
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2022-08-06 18:58:33 +0200

    ci: add release step

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+	plugins: [
+		dts({
+			insertTypesEntry: true,
+		}),
+	],
+	build: {
+		lib: {
+			entry: './src/index.ts',
+			name: 'zod-fixture',
+			formats: ['es', 'umd'],
+			fileName: format => `zod-fixture.${format}.js`,
+		},
+	},
+});
