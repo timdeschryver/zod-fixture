81db444457f81aa8851e3a57b88fcc58ee85aab4
Chad Elliott <celliott@nipr.com>
2023-05-21 16:11:16 -0500

    allow extending of default generators

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
+import { Definition } from '@/core/generator';
 import { Config, Core } from './core/core';
 import defaultGenerators from './generators/default';
 
+export { Generator } from '@/core/generator';
+
 export function generate<TSchema extends Zod.ZodTypeAny>(
 	schema: TSchema,
-	config: Config = {},
+	config: Config & {
+		extend?: Definition<any>[];
+	} = {},
 ): unknown {
-	return new Core(config).register(defaultGenerators).generate(schema);
+	return new Core(config)
+		.register(defaultGenerators)
+		.register(config.extend ?? [])
+		.generate(schema);
 }
