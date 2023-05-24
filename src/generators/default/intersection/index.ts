4fd5ae4afa9f1fe9f11e614b76549b7f30f8f0b5
Chad Elliott <celliott@nipr.com>
2023-05-24 03:03:09 -0500

    copied zod merge strategy and added test for array

--- src/generators/default/intersection/index.ts
+++ src/generators/default/intersection/index.ts
@@ -1,5 +1,5 @@
 import { Generator } from '@/core/generator';
-import { ZodIntersection } from 'zod';
+import { ZodIntersection, ZodParsedType, getParsedType, util } from 'zod';
 
 export const IntersectionGenerator = Generator({
 	schema: ZodIntersection,
@@ -7,7 +7,67 @@ export const IntersectionGenerator = Generator({
 	output: ({ def, core }) => {
 		const left = core.generate(def.left);
 		const right = core.generate(def.right);
+		const result = mergeValues(left, right);
 
-		return Object.assign({}, left, right);
+		if (!result.valid) throw new Error('Intersection is not valid.');
+		return result.data;
 	},
 });
+
+/**
+ * Merge strategy copied from Zod itself.
+ */
+function mergeValues(
+	a: any,
+	b: any,
+): { valid: true; data: any } | { valid: false } {
+	const aType = getParsedType(a);
+	const bType = getParsedType(b);
+
+	if (a === b) {
+		return { valid: true, data: a };
+	} else if (aType === ZodParsedType.object && bType === ZodParsedType.object) {
+		const bKeys = util.objectKeys(b);
+		const sharedKeys = util
+			.objectKeys(a)
+			.filter(key => bKeys.indexOf(key) !== -1);
+
+		const newObj: any = { ...a, ...b };
+		for (const key of sharedKeys) {
+			const sharedValue = mergeValues(a[key], b[key]);
+			if (!sharedValue.valid) {
+				return { valid: false };
+			}
+			newObj[key] = sharedValue.data;
+		}
+
+		return { valid: true, data: newObj };
+	} else if (aType === ZodParsedType.array && bType === ZodParsedType.array) {
+		if (a.length !== b.length) {
+			return { valid: false };
+		}
+
+		const newArray = [];
+		for (let index = 0; index < a.length; index++) {
+			const itemA = a[index];
+			const itemB = b[index];
+			const sharedValue = mergeValues(itemA, itemB);
+
+			if (!sharedValue.valid) {
+				return { valid: false };
+			}
+
+			newArray.push(sharedValue.data);
+		}
+
+		return { valid: true, data: newArray };
+	} else if (
+		aType === ZodParsedType.date &&
+		bType === ZodParsedType.date &&
+		+a === +b
+	) {
+		return { valid: true, data: a };
+	} else {
+		return { valid: false };
+	}
+}
--- src/generators/default/intersection/intersection.test.ts
+++ src/generators/default/intersection/intersection.test.ts
@@ -2,13 +2,14 @@ import { Core } from '@/core/core';
 import { describe, expect, test } from 'vitest';
 import { z } from 'zod';
 import { IntersectionGenerator } from '.';
+import { ArrayGenerator } from '../array';
 import { BooleanGenerator } from '../boolean';
 import { ObjectGenerator } from '../object';
 
 describe('create intersections', () => {
-	const core = new Core().register([IntersectionGenerator, ObjectGenerator, BooleanGenerator]);
+	const core = new Core().register([IntersectionGenerator, ObjectGenerator, BooleanGenerator, ArrayGenerator]);
 
-	test('creates an intersection', () => {
+	test('creates an intersection of objects', () => {
 		const left = z.object({
 			left: z.boolean(),
 		});
@@ -27,4 +28,20 @@ describe('create intersections', () => {
             right: expect.any(Boolean)
         })
 	});
+
+    test('creates an intersection of arrays', () => {
+        const left = z.object({
+			left: z.boolean(),
+		}).array();
+
+		const right = z.object({
+			right: z.boolean(),
+		}).array();
+
+		const intersection = z.intersection(left, right);
+		const result = core.generate(intersection) as any[];
+
+		expect(result[0]).toHaveProperty('left');
+		expect(result[0]).toHaveProperty('right');
+    })
 });
