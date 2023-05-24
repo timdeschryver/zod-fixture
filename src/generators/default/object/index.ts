8daccdd36cbac9158dcc1771ebd3f78bf87765aa
Chad Elliott <celliott@nipr.com>
2023-05-24 03:31:56 -0500

    updated record builder and ctx

--- src/generators/default/object/index.ts
+++ src/generators/default/object/index.ts
@@ -1,16 +1,16 @@
 import { Generator } from '@/core/generator';
-import { ZodObject, ZodRecord } from 'zod';
+import { ZodObject, ZodRecord, z } from 'zod';
 
 export const ObjectGenerator = Generator({
 	schema: ZodObject,
 	matches: () => true,
-	output: ({ def, core }) => {
+	output: ({ def, core, ctx }) => {
 		const shape = def.shape();
 		const result: Record<string, unknown> = {};
 
 		for (const key in shape) {
 			const type = shape[key];
-			if (type) result[key] = core.generate(type, { key });
+			if (type) result[key] = core.generate(type, { path: [...ctx.path, key] });
 		}
 
 		return result;
@@ -20,5 +20,19 @@ export const ObjectGenerator = Generator({
 export const RecordGenerator = Generator({
 	schema: ZodRecord,
 	matches: () => true,
-	output: ({ def, core }) => core.utils.n(() => core.generate(def.valueType)),
+	output: ({ def, core, ctx }) => {
+		const result: Record<
+			z.infer<typeof def.keyType>,
+			z.infer<typeof def.valueType>
+		> = {};
+
+		core.utils.n(() => {
+			const key = core.generate(def.keyType, ctx) as string | number;
+			const value = core.generate(def.valueType, { path: [...ctx.path, key] });
+
+			result[key] = value;
+		});
+
+		return result;
+	},
 });
