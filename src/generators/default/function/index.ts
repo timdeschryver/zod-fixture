e54abf2bded73c344ebfa3a78bcc0278c627a114
Chad Elliott <celliott@nipr.com>
2023-05-24 04:30:30 -0500

    dead code

--- src/generators/default/function/index.ts
+++ src/generators/default/function/index.ts
@@ -1,9 +1,6 @@
 import { Generator } from '@/core/generator';
 import { ZodFunction } from 'zod';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const noop: () => void = () => {};
-
 export const FunctionGenerator = Generator({
 	schema: ZodFunction,
 	matches: () => true,
