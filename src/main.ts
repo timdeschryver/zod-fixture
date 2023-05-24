3c2b55f9949f8346b09a6b8fa00f7ce322e38617
Chad Elliott <celliott@nipr.com>
2023-05-23 23:06:37 -0500

    exported symbols for development

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { z } from 'zod';
 import { generate } from '.';
+import { ZOD_INSTANCE_IDENTIFIER, ZOD_TYPE_IDENTIFIER } from './core/generator';
 
 const win = window as any
 
+win.ZOD_TYPE_IDENTIFIER = ZOD_TYPE_IDENTIFIER;
+win.ZOD_INSTANCE_IDENTIFIER = ZOD_INSTANCE_IDENTIFIER;
 win.z = z;
 win.generate = generate;
