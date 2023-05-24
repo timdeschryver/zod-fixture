28e6166574676c84fb2295c95c993a1016c13751
Chad Elliott <celliott@nipr.com>
2023-05-24 05:15:59 -0500

    more robust any type

--- src/generators/default/any/index.ts
+++ src/generators/default/any/index.ts
@@ -1,10 +1,27 @@
 import { Generator } from '@/core/generator';
 import { ZodAny, z } from 'zod';
 
-const OPTIONS = [z.string(), z.number(), z.boolean(), z.undefined(), z.null()];
+const POTENTIAL_SCHEMAS = [
+	z.undefined(),
+	z.null(),
+	z.boolean(),
+	z.number(),
+	z.string(),
+	z.bigint(),
+	z.date(),
+	z.symbol(),
+	z.unknown(),
+	z.nan(),
+	z.record(z.string()), //`z.object` is just a subset of this - no need for a separate case.
+	z.array(z.string()), //Tuples are just a subset of this - no need for a separate case.
+	z.map(z.string(), z.string()),
+	z.set(z.string()),
+	z.promise(z.string()),
+];
 
 export const AnyGenerator = Generator({
 	schema: ZodAny,
 	matches: () => true,
-	output: ({ core, ctx }) => core.generate(core.utils.randomFrom(OPTIONS), ctx),
+	output: ({ core, ctx }) =>
+		core.generate(core.utils.randomFrom(POTENTIAL_SCHEMAS), ctx),
 });
