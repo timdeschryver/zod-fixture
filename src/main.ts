28172f234773a8a08e75fc8c2f91e5bb3ba8caf8
Chad Elliott <celliott@nipr.com>
2023-06-14 21:27:37 -0500

    updated exports for repl experimentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
+import MersenneTwister from '@/core/MersenneTwister';
 import { z } from 'zod';
-import { generate } from '.';
+import { createFixture } from '.';
 import { ZOD_INSTANCE_IDENTIFIER, ZOD_TYPE_IDENTIFIER } from './core/generator';
 
-const win = window as any
+const win = window as any;
 
 win.ZOD_TYPE_IDENTIFIER = ZOD_TYPE_IDENTIFIER;
 win.ZOD_INSTANCE_IDENTIFIER = ZOD_INSTANCE_IDENTIFIER;
 win.z = z;
-win.generate = generate;
+win.createFixture = createFixture;
+win.MersenneTwister = MersenneTwister;
