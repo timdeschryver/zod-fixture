4bb8ba34c50ccf782562ec69202d2b4157cc76f0
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2023-06-22 18:14:50 +0200

    test: generator order

--- test/core.test.ts
+++ test/core.test.ts
@@ -1,7 +1,9 @@
 import { describe, expect, test } from 'vitest';
-import { z } from 'zod';
-import { Core } from '../src';
+import { ZodNumber, z } from 'zod';
+import { Core, Generator } from '../src';
 import defaultGenerators from '../src/generators/default';
+import { NumberGenerator } from '../src/generators/default/number';
+import { ObjectGenerator } from '../src/generators/default/object';
 import { StringGenerator } from '../src/generators/default/string';
 
 describe('core', () => {
@@ -48,5 +50,49 @@ describe('core', () => {
 		expect(() => core.generate(PersonSchema)).toThrow(
 			/No generator found for ZodString/i
 		);
+	});
+
+	describe('order of generators', () => {
+		const FooNumberGenerator = Generator({
+			schema: ZodNumber,
+			matches: (x) => x.context?.path?.includes('foo'),
+			output: () => {
+				return 4;
+			},
+		});
+
+		test(`test one`, () => {
+			const core = new Core().register([
+				ObjectGenerator,
+				FooNumberGenerator,
+				NumberGenerator,
+			]);
+
+			const result = core.generate(
+				z.object({
+					foo: z.number(),
+					other: z.number(),
+				})
+			);
+
+			expect(result.foo).toBe(4);
+		});
+
+		test.fails(`test two`, () => {
+			const core = new Core().register([
+				ObjectGenerator,
+				NumberGenerator,
+				FooNumberGenerator,
+			]);
+
+			const result = core.generate(
+				z.object({
+					foo: z.number(),
+					other: z.number(),
+				})
+			);
+
+			expect(result.foo).toBe(4);
+		});
 	});
 });
