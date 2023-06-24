425358440ef2fc2bc1fc387f0e2326ecd63b2cca
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2023-06-24 20:52:26 +0200

    fix multiple of

--- src/generators/default/number/index.ts
+++ src/generators/default/number/index.ts
@@ -7,27 +7,30 @@ export const NumberGenerator = Generator({
 		const { checks } = def;
 		const { filter } = core.utils;
 
-		let min = filter.checks(checks, 'min')?.value ?? core.defaults.float.min;
-		let max = filter.checks(checks, 'max')?.value ?? core.defaults.float.max;
+		const min = filter.checks(checks, 'min')?.value ?? core.defaults.float.min;
+		const max = filter.checks(checks, 'max')?.value ?? core.defaults.float.max;
 
 		const multipleOf = filter.checks(checks, 'multipleOf')?.value;
 		const isInt = filter.checks(checks, 'int') !== undefined;
 		const isFinite = filter.checks(checks, 'finite') !== undefined;
-
-		if (multipleOf !== undefined) {
-			min = min / multipleOf;
-			max = max / multipleOf;
-		}
 
 		let result = isInt
 			? core.utils.random.int({ min, max })
 			: core.utils.random.float({ min, max });
 
 		if (multipleOf !== undefined) {
-			result = result * multipleOf;
+			result = Math.round(result / multipleOf) * multipleOf;
 
-			if (multipleOf % 1 === 0) {
-				result = Math.round(result);
+			if (result < min) {
+				result += multipleOf;
+			}
+			if (result > max) {
+				result -= multipleOf;
+			}
+
+			if (multipleOf % 1 !== 0) {
+				const decimals = multipleOf.toString().split('.')[1]?.length;
+				result = Number(result.toFixed(decimals));
 			}
 		}
 
--- src/generators/default/number/number.test.ts
+++ src/generators/default/number/number.test.ts
@@ -18,60 +18,72 @@ describe('create numbers', () => {
 		expect(core.generate(z.number().int())).toBeTypeOf('number');
 	});
 
-	test('creates a number with a min value', () => {
-		expect(core.generate(z.number().min(10_000))).toBeGreaterThanOrEqual(
-			10_000
-		);
-		expect(core.generate(z.number().gte(10_000))).toBeGreaterThanOrEqual(
-			10_000
-		);
+	describe('min and max', () => {
+		test('creates a number with a min value', () => {
+			expect(core.generate(z.number().min(10_000))).toBeGreaterThanOrEqual(
+				10_000
+			);
+			expect(core.generate(z.number().gte(10_000))).toBeGreaterThanOrEqual(
+				10_000
+			);
+		});
+
+		test('creates a number with a negative min value', () => {
+			expect(core.generate(z.number().min(-10))).toBeGreaterThanOrEqual(-10);
+		});
+
+		test('creates a number with a max value', () => {
+			expect(core.generate(z.number().max(5))).toBeLessThanOrEqual(5);
+			expect(core.generate(z.number().lte(5))).toBeLessThanOrEqual(5);
+		});
+
+		test('creates a number with a negative max value', () => {
+			expect(core.generate(z.number().max(-5))).toBeLessThanOrEqual(-5);
+		});
+
+		test('creates a number with a negative max value', () => {
+			const result = core.generate(z.number().min(-10).max(-5));
+			expect(result).toBeGreaterThanOrEqual(-10);
+			expect(result).toBeLessThanOrEqual(-5);
+		});
+
+		test('throws when min is greater than max', () => {
+			expect(() => core.generate(z.number().min(100).max(10))).toThrowError();
+		});
 	});
 
-	test('creates a number with a negative min value', () => {
-		expect(core.generate(z.number().min(-10))).toBeGreaterThanOrEqual(-10);
+	describe('positive and negative', () => {
+		test('creates a positive number', () => {
+			expect(core.generate(z.number().positive())).toBeGreaterThan(0);
+			expect(core.generate(z.number().nonnegative())).toBeGreaterThanOrEqual(0);
+		});
+
+		test('creates a negative number', () => {
+			expect(core.generate(z.number().negative())).toBeLessThan(0);
+			expect(core.generate(z.number().nonpositive())).toBeLessThanOrEqual(0);
+		});
 	});
 
-	test('creates a number with a max value', () => {
-		expect(core.generate(z.number().max(5))).toBeLessThanOrEqual(5);
-		expect(core.generate(z.number().lte(5))).toBeLessThanOrEqual(5);
-	});
+	describe('multipleOf', () => {
+		test("creates a number that's a multiple", () => {
+			const schema = z.number().multipleOf(4);
+			const result = core.generate(schema);
+			expect(() => schema.parse(result)).not.toThrow();
+		});
 
-	test('creates a number with a negative max value', () => {
-		expect(core.generate(z.number().max(-5))).toBeLessThanOrEqual(-5);
-	});
+		test("creates a number that's a multiple with min and max", () => {
+			expect(core.generate(z.number().multipleOf(3).min(16).max(20))).toBe(18);
+		});
 
-	test('creates a number with a negative max value', () => {
-		const result = core.generate(z.number().min(-10).max(-5));
-		expect(result).toBeGreaterThanOrEqual(-10);
-		expect(result).toBeLessThanOrEqual(-5);
-	});
-
-	test('creates a positive number', () => {
-		expect(core.generate(z.number().positive())).toBeGreaterThan(0);
-		expect(core.generate(z.number().nonnegative())).toBeGreaterThanOrEqual(0);
-	});
-
-	test('creates a negative number', () => {
-		expect(core.generate(z.number().negative())).toBeLessThan(0);
-		expect(core.generate(z.number().nonpositive())).toBeLessThanOrEqual(0);
-	});
-
-	test("creates a number that's a multiple", () => {
-		expect(core.generate(z.number().multipleOf(3).min(16).max(20))).toBe(19);
-	});
-
-	test("creates a number that's a multiple of a decimal", () => {
-		expect(
-			core.generate(z.number().multipleOf(3.3).min(16).max(20))
-		).toBeCloseTo(19, 0);
+		test("creates a number that's a multiple of a decimal", () => {
+			expect(core.generate(z.number().multipleOf(3.3).min(16).max(19))).toBe(
+				16.5
+			);
+		});
 	});
 
 	test('creates a finite number', () => {
-		expect(isFinite(core.generate(z.number().finite()))).toBeTruthy();
-	});
-
-	test('throws when min is greater than max', () => {
-		expect(() => core.generate(z.number().min(100).max(10))).toThrowError();
+		expect(isFinite(core.generate(z.number().finite()) as number)).toBeTruthy();
 	});
 
 	test("creates a number that's a multiple", () => {
