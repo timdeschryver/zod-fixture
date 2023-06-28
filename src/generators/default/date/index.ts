77b25f93c48de2324b18c19646889e93399d0eac
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2023-06-28 17:24:55 +0000

    fix: create date in utc

--- src/generators/default/date/index.ts
+++ src/generators/default/date/index.ts
@@ -9,6 +9,8 @@ export const DateGenerator = Generator({
 		const min = checks.find('min')?.value ?? core.defaults.date.min.getTime();
 		const max = checks.find('max')?.value ?? core.defaults.date.max.getTime();
 
-		return new Date(core.utils.random.int({ min, max }));
+		const date = new Date(0);
+		date.setUTCMilliseconds(core.utils.random.int({ min, max }));
+		return date;
 	},
 });
