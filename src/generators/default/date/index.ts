51122003665a7c81937b0bf79e7d41a178cd4668
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2023-06-28 19:31:20 +0200

    revert

--- src/generators/default/date/index.ts
+++ src/generators/default/date/index.ts
@@ -9,8 +9,6 @@ export const DateGenerator = Generator({
 		const min = checks.find('min')?.value ?? core.defaults.date.min.getTime();
 		const max = checks.find('max')?.value ?? core.defaults.date.max.getTime();
 
-		const date = new Date(core.utils.random.int({ min, max }));
-		const userTimezoneOffset = date.getTimezoneOffset() * 60000;
-		return new Date(date.getTime() - userTimezoneOffset);
+		return new Date(core.utils.random.int({ min, max }));
 	},
 });
