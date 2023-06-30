924c8dcc1c56b88b1813e1a9174260525da41dbf
Chad Elliott <celliott@nipr.com>
2023-06-30 16:34:28 -0500

    browser based "testing" moved to test folder

--- test/browser.ts
+++ test/browser.ts
@@ -1,10 +1,10 @@
-import MersenneTwister from '@/transformer/utils/MersenneTwister';
-import { z } from 'zod';
-import { Fixture, Generator, Transformer } from './export';
+import { Fixture, Generator, Transformer } from '@/public';
 import {
 	ZOD_INSTANCE_IDENTIFIER,
 	ZOD_TYPE_IDENTIFIER,
-} from './transformer/generator';
+} from '@/transformer/generator';
+import MersenneTwister from '@/transformer/utils/MersenneTwister';
+import { z } from 'zod';
 
 Object.assign(window, {
 	Generator,
