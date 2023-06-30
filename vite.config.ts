0e27aef14fce7aebc48fb3ed0a8391d4301f2fa2
Chad Elliott <celliott@nipr.com>
2023-06-30 16:56:12 -0500

    fixed aliases for zod-fixture import

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 		alias: {
 			// eslint-disable-next-line no-undef
 			'@': path.resolve(__dirname, './src'),
-			'zod-fixture': path.resolve(__dirname, './src'),
+			'zod-fixture': path.resolve(__dirname, './src/public.ts'),
 		},
 	},
 	plugins: [
