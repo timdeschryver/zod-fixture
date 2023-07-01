bb86fdb033a6fc2b62aa3292e1b628bb6718b935
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2023-07-01 07:05:24 +0000

    docs: remove base url

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vitepress';
 export default defineConfig({
 	title: 'zod-fixture',
 	description: 'Documentation for zod-fixture',
-	base: '/zod-fixture/',
 	lang: 'en-US',
 	lastUpdated: true,
 	cleanUrls: true,
