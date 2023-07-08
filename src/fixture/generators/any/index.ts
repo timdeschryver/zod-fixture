67412c6f6dc0fa81c342e002ee3e008366077cc4
Chad Elliott <celliott@nipr.com>
2023-07-08 11:33:23 -0500

    zodany is in an acceptable form

--- src/fixture/generators/any/index.ts
+++ src/fixture/generators/any/index.ts
@@ -1,25 +1,8 @@
 import { ZodAny } from '@/internal/zod';
 import { Generator } from '@/transformer/generator';
 
-// @TODO: refactor this so we don't need to include the full zod library.
-// const POTENTIAL_SCHEMAS = [
-// 	z.undefined(),
-// 	z.null(),
-// 	z.boolean(),
-// 	z.number(),
-// 	z.string(),
-// 	z.bigint(),
-// 	z.date(),
-// 	z.symbol(),
-// 	z.nan(),
-// 	z.record(z.string()), //`z.object` is just a subset of this - no need for a separate case.
-// 	z.array(z.string()), //Tuples are just a subset of this - no need for a separate case.
-// 	z.map(z.string(), z.string()),
-// 	z.set(z.string()),
-// 	z.promise(z.string()),
-// ];
-
 export const AnyGenerator = Generator({
 	schema: ZodAny,
+	// @TODO: implement a more robust any generator.
 	output: () => 'ZodAny',
 });
