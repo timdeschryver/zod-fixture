a14608b14bbfcda7613367ec8d4767eff372b244
Chad Elliott <celliott@nipr.com>
2023-07-12 15:37:29 -0500

    more appropriately extend Transformer classes

--- src/fixture/fixture.ts
+++ src/fixture/fixture.ts
@@ -1,10 +1,24 @@
 import type { z } from 'zod';
 import type { Defaults } from '../transformer/defaults';
-import { constrained, unconstrained } from '../transformer/defaults';
-import { Transformer } from '../transformer/transformer';
+import {
+	ConstrainedTransformer,
+	UnconstrainedTransformer,
+} from '../transformer/transformer';
 import { fixtureGenerators } from './generators';
 
-export interface ConstrainedFixture extends Transformer {
+function missingGeneratorError(schema: z.ZodTypeAny) {
+	const message = [
+		`No generator found for ${schema.constructor.name}.`,
+		'',
+		'For z.custom, refer to the documentation https://github.com/timdeschryver/zod-fixture.',
+		'If you still believe this is an error, please open an issue at https://github.com/timdeschryver/zod-fixture/issues/new.',
+		'',
+	].join('\n');
+
+	return new Error(message);
+}
+
+export interface ConstrainedFixture extends ConstrainedTransformer {
 	// explicitly define the return type
 	fromSchema<TSchema extends z.ZodTypeAny>(
 		schema: TSchema,
@@ -12,25 +26,22 @@ export interface ConstrainedFixture extends Transformer {
 	): z.infer<TSchema>;
 }
 
-export class ConstrainedFixture extends Transformer {
+export class ConstrainedFixture extends ConstrainedTransformer {
 	generators = fixtureGenerators;
-	transformerConfig = constrained;
-
-	missingGeneratorError(schema: z.ZodTypeAny) {
-		const message = [
-			`No generator found for ${schema.constructor.name}.`,
-			'',
-			'For z.custom, refer to the documentation https://github.com/timdeschryver/zod-fixture.',
-			'If you still believe this is an error, please open an issue at https://github.com/timdeschryver/zod-fixture/issues/new.',
-			'',
-		].join('\n');
-
-		return new Error(message);
-	}
+	missingGeneratorError = missingGeneratorError;
 }
 
-export class UnconstrainedFixture extends ConstrainedFixture {
-	transformerConfig = unconstrained;
+export interface UnconstrainedFixture extends UnconstrainedTransformer {
+	// explicitly define the return type
+	fromSchema<TSchema extends z.ZodTypeAny>(
+		schema: TSchema,
+		userDefaults?: Partial<Defaults>
+	): z.infer<TSchema>;
+}
+
+export class UnconstrainedFixture extends UnconstrainedTransformer {
+	generators = fixtureGenerators;
+	missingGeneratorError = missingGeneratorError;
 }
 
 export { ConstrainedFixture as Fixture };
