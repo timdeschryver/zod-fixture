fee100a1f4ff93312eb606c9871ef77959f48932
Chad Elliott <celliott@nipr.com>
2023-07-11 23:35:29 -0500

    changed order and added comments for clarity

--- examples/create-fixture-extension.test.ts
+++ examples/create-fixture-extension.test.ts
@@ -3,22 +3,40 @@ import { expect, test } from 'vitest';
 import { ZodNumber, ZodObject, z } from 'zod';
 import { Fixture, Generator } from 'zod-fixture';
 
+// #region generator
+const totalVisitsGenerator = Generator({
+	schema: ZodNumber,
+	filter: ({ context }) => context.path.at(-1) === 'totalVisits',
+	/**
+	 * The `context` provides a path to the current field
+	 *
+	 * {
+	 *   totalVisits: ...,
+	 *   nested: {
+	 *     totalVisits: ...,
+	 *   }
+	 * }
+	 *
+	 * Would match twice with the following paths:
+	 *   ['totalVisits']
+	 *   ['nested', 'totalVisits']
+	 */
+
+	// returns a more realistic number of visits.
+	output: ({ transform }) => transform.utils.random.int({ min: 0, max: 25 }),
+});
+// #endregion generator
+
 const addressGenerator = Generator({
 	schema: ZodObject,
 	filter: ({ context }) => context.path.at(-1) === 'address',
+	// returns a custom address object
 	output: () => ({
 		street: 'My Street',
 		city: 'My City',
 		state: 'My State',
 	}),
 });
-
-// #region generator
-const totalVisitsGenerator = Generator({
-	schema: ZodNumber,
-	output: ({ transform }) => transform.utils.random.int({ min: 0, max: 25 }),
-});
-// #endregion generator
 
 const PersonSchema = z.object({
 	name: z.string(),
