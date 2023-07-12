0f474f4ea2449af751d14d9527d0d4e567175f1a
Chad Elliott <celliott@nipr.com>
2023-07-12 15:58:41 -0500

    put min constraint on visits for better comprehension

--- examples/create-fixture-extension.test.ts
+++ examples/create-fixture-extension.test.ts
@@ -29,7 +29,7 @@ const PersonSchema = z.object({
 		state: z.string(),
 	}),
 	pets: z.array(z.object({ name: z.string(), breed: z.string() })),
-	totalVisits: z.number().int(),
+	totalVisits: z.number().int().min(0),
 });
 
 const fixture = new Fixture({ seed: 38 }).extend([
