752ae5b1e72245295e4139b1de0cdee782cfb42a
Chad Elliott <celliott@nipr.com>
2023-07-22 14:50:19 -0500

    moved type predicate to utils

--- src/transformer/utils/index.ts
+++ src/transformer/utils/index.ts
@@ -1,4 +1,6 @@
 import type { ZodTypeAny } from 'zod';
+import type { ZodConstructorOrSchema } from '../generator';
+import { isZodConstructor, ZOD_INSTANCE_IDENTIFIER } from '../generator';
 import type { Runner } from '../runner';
 import { Checks } from './Checks';
 import { Randomization } from './Randomization';
@@ -59,6 +61,32 @@ export class Utils {
 	) {
 		if (!schema || schema._def.typeName === 'ZodNever') return;
 		assignment(schema);
+	}
+
+	isType<TSchema extends ZodTypeAny>(
+		target: ZodConstructorOrSchema<TSchema> | undefined,
+		schema: ZodTypeAny
+	): schema is TSchema {
+		if (!target) return false;
+
+		if (isZodConstructor(target)) {
+			if (schema._def.typeName !== target.name) {
+				return false;
+			}
+		} else {
+			if (schema._def.typeName !== target._def.typeName) {
+				return false;
+			}
+
+			// If our generator was created with an instance, make sure it matches
+			// the schema we're trying to generate.
+			// This is particularly important for z.custom schemas.
+			if (schema[ZOD_INSTANCE_IDENTIFIER] !== undefined) {
+				if (schema[ZOD_INSTANCE_IDENTIFIER] !== target[ZOD_INSTANCE_IDENTIFIER])
+					return false;
+			}
+		}
+		return true;
 	}
 
 	checks<TChecks extends { kind: string }[]>(checks: TChecks) {
