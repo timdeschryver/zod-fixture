b73737c45efbb2f619fea97a59b05c4aa238acef
Chad Elliott <celliott@nipr.com>
2023-08-01 01:36:23 -0500

    feat: Deterministic Boolean Generation
    The default boolean generator's initial state is now based off the seed to provide more deterministic results.

--- src/fixture/generators/boolean/index.ts
+++ src/fixture/generators/boolean/index.ts
@@ -1,9 +1,7 @@
 import { ZodBoolean } from '@/internal/zod';
 import { Generator } from '@/transformer/generator';
 
-// eslint-disable-next-line prefer-const
-let toggle = false;
 export const BooleanGenerator = Generator({
 	schema: ZodBoolean,
-	output: () => (toggle = !toggle),
+	output: ({ transform }) => transform.utils.random.boolean(),
 });
