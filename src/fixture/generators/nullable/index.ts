7335f0e3acd2d0f57185c385ad7fd4abd49e64ab
Chad Elliott <celliott@nipr.com>
2023-08-06 07:55:09 -0500

    feat: nullable is now psuedo random

--- src/fixture/generators/nullable/index.ts
+++ src/fixture/generators/nullable/index.ts
@@ -6,9 +6,11 @@ export const NullableGenerator = Generator({
 	output: ({ def, transform, context }) => {
 		let result = null;
 
-		transform.utils.recursionCheck(def.innerType, () => {
-			result = transform.fromSchema(def.innerType, context);
-		});
+		if (transform.utils.random.boolean()) {
+			transform.utils.recursionCheck(def.innerType, () => {
+				result = transform.fromSchema(def.innerType, context);
+			});
+		}
 
 		return result;
 	},
